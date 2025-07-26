bf898267ad1a5bdef4557801e72f616768b7055f
wangzhaoyu <wangzhaoyu@yogorobot.com>
2025-07-26 19:05:26 +0800

    Updates

--- src/contexts/authContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const AuthContext = createContext({
-  isAuthenticated: false,
-  setIsAuthenticated: (value: boolean) => {},
-  logout: () => {},
-});
